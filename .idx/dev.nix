7210fc9626b69fd8179820a1a4e2e0095340f63d
alphainno61 <alphainno61@gmail.com>
2025-11-15 11:39:21 +0000

    feat: hello

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,18 +6,7 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
-    pkgs.zulu
   ];
-  # Sets environment variables in the workspace
-  env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
-  services.firebase.emulators = {
-    # Disabling because we are using prod backends right now
-    detect = false;
-    projectId = "demo-app";
-    services = ["auth" "firestore"];
-  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -35,7 +24,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = ["sh" "-c" "npm install && npm run dev -- --port $PORT --hostname 0.0.0.0"];
           manager = "web";
         };
       };
